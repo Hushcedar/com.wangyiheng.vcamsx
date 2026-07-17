c4a443bff9f5bf7f667f170e3cd40f7202bb6efc
Hushcedar <hushcedar@gmail.com>
2026-07-17 09:20:04 +0100

    Reorder plugins block in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,3 @@
-plugins {
-    id("com.android.application") version "8.1.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
-}
-
 buildscript {
     repositories {
         google()
@@ -12,4 +7,9 @@ buildscript {
         classpath("com.android.tools.build:gradle:8.1.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
     }
+}
+
+plugins {
+    id("com.android.application") version "8.1.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
 }
