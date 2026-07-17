6b3a2a6fd70ed32f6658d966973602d13a77a51e
Hushcedar <hushcedar@gmail.com>
2026-07-17 09:10:14 +0100

    Change root project name and repository configuration
    Updated project name and modified repository settings.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,21 +1,10 @@
-pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
-        gradlePluginPortal()
         mavenCentral()
-        maven("https://api.xposed.info/")
-        maven ("https://maven.pkg.github.com/GCX-HCI/tray" )
+        maven("https://jitpack.io")
     }
 }
 
-rootProject.name = "VCAMSX"
+rootProject.name = "com.wangyiheng.vcamsx"
 include(":app")
- 
